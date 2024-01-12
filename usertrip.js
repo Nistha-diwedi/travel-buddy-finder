6332b117fdb5332eeb1c85d5a81da64a712d434d
Nistha Diwedi <118626112+Nistha-diwedi@users.noreply.github.com>
2024-01-12 21:04:06 +0530

    Add files via upload

---
+++ usertrip.js
@@ -0,0 +1,12 @@
+var startdateoftrip = document.getElementById("startdateoftrip").value;
+var enddateoftrip = document.getElementById("enddateoftrip").value;
+var sourcestartpoint = document.getElementById("sourcestartpoint").value;
+var sourceendpoint = document.getElementById("sourceendpoint").value;
+var budgetoftrip = document.getElementById("budgetoftrip").value;
+var foodintripshouldbe = document.getElementById("foodintripshouldbe").value;
+var noofpeoplesintrip = document.getElementById("noofpeoplesintrip").value;
+
+
+function findpeoplesandsavetrip() {
+
+}
